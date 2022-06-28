71cbf381ad909b15363e4ed2635e3725099ebf40
yoonk1228 <yoonk1228@naver.com>
2022-06-28 12:24:34 +0900

    feat: 트랜잭션 내용 사용 후 확인

--- 20220628-web3/readme.md
+++ 20220628-web3/readme.md
@@ -35,6 +35,8 @@ describe('web3 테스트 코드',()=>{
 
 ```sh
 npm i web3
+npm i ethereumjs-tx
+# Transaction 객체를 이더리움 클라이언트가 이해할 수 있게 만들어 준다.
 ```
 **web3 문서**
 `https://ethereum.github.io/execution-apis/api-documentation/`
--- 20220628-web3/web3.test.js
+++ 20220628-web3/web3.test.js
@@ -1,4 +1,5 @@
 const Web3 = require('web3')
+const ethTx = require('ethereumjs-tx').Transaction
 
 describe('web3 테스트 코드',()=>{
   let web3
@@ -45,5 +46,52 @@ describe('web3 테스트 코드',()=>{
     // wei
     console.log(web3.utils.toWei('1','gwei'))
     console.log(web3.utils.toWei('1','ether'))
+  })
+  it('트랜잭션 횟수 구해오기', async ()=>{
+    const txCount = await web3.eth.getTransactionCount(sender)
+    console.log(txCount)
+    console.log(web3.utils.toHex(txCount))
+  })
+  it('트랜잭션 실행하기', async ()=>{
+    // 보내는사람의 tx 를 counting 한다.
+    // Nonce : tx 에 계정이 가지고 있는 고유한 값
+    const txCount = await web3.eth.getTransactionCount(sender)
+    // sender : 0x751e8e1755aa1f7046d96896517a04750605871d67ba94a515f8bd578f5fb56b
+    const privateKey = Buffer.from('751e8e1755aa1f7046d96896517a04750605871d67ba94a515f8bd578f5fb56b','hex')
+    const txObject = {
+      nonce: web3.utils.toHex(txCount),
+      from: sender,
+      to: received,
+      value: web3.utils.toHex(web3.utils.toWei('1','ether')), // 보낼 금액 -> 10**18 => toHex
+      gasLimit: web3.utils.toHex(6721975), // todo: ganache 에 evm_snapshot 적용하기
+      gasPrice: web3.utils.toHex(web3.utils.toWei('1','gwei')),
+      data: web3.utils.toHex('')
+    }
+    const tx = new ethTx(txObject)
+    // console.log('sign 이전',tx)
+    tx.sign(privateKey)
+    // console.log('sign 이후',tx)
+    const serializedTx = tx.serialize()
+    // console.log(serializedTx.toString('hex'))
+    const transactionObject = await web3.eth.sendSignedTransaction('0x'+serializedTx.toString('hex'))
+    console.log(transactionObject)
+  })
+  it('Balance 확인', async ()=>{
+    const senderBalance = await web3.eth.getBalance(sender)
+    const receivedBalance = await web3.eth.getBalance(received)
+
+    console.log('sender balance : ', senderBalance / 10 ** 18)
+    console.log('received balance : ', receivedBalance / 10 ** 18)
+  })
+  it('가스 사용량 확인하기',()=>{
+    //  가스 사용량 21004
+    //  21000 + 4
+
+    //  가스 사용량 21004
+    //  가스 가격 1 = 1gwei
+    //  가스최대치 6721975
+
+    //  1 트랜잭션 당 얼마의 값이 나가는지
+    //  1 - 총 사용량( 총 사용량 * 가스 가격 ) === 수수료 값
   })
 })
